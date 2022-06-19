249d8ba7065aa9c369a1e4c4c8cec5de2217cf63
m.chudakova <masterova89@mail.ru>
2022-06-19 11:37:14 +0300

    Initial Commit

---
+++ TrafficLite/TrafficLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLiteApp.swift
+//  TrafficLite
+//
+//  Created by Мария Чудакова on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
